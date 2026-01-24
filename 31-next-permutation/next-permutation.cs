4b31ed8c7e7aaca3c0889382f06daae0857c07e6
Prateek Narain <103872446+PrateekNarain@users.noreply.github.com>
2026-01-24 19:15:49 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (57.78%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    public void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    public void NextPermutation(int[] nums) {
+        int n = nums.Length;
+        int i = n - 2;
+
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+        reverse(nums, i + 1, n - 1);
+    }
+}
