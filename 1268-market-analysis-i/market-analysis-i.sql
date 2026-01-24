0fdbef2f63f62c26d7c90527d0cf77fc211067cd
Prateek Narain <103872446+PrateekNarain@users.noreply.github.com>
2026-01-24 11:59:25 +0530

    Time: 1230 ms (39.47%) | Memory: 0B (100.00%) - LeetSync

--- 1268-market-analysis-i/market-analysis-i.sql
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -1,10 +1,11 @@
-# Write your MySQL query statement below
-SELECT u.user_id AS buyer_id, u.join_date AS join_date, IFNULL(o.orders_19,0) AS orders_in_2019
+SELECT 
+    u.user_id AS buyer_id,
+    u.join_date,
+    COUNT(o.order_id) AS orders_in_2019
 FROM Users u
-LEFT JOIN (
-    SELECT buyer_id, (SUM(CASE WHEN order_date BETWEEN '2019-01-01' AND '2019-12-31' THEN 1 ELSE 0 END)) AS orders_19
-    FROM Orders
-    GROUP BY buyer_id
-) o
-ON u.user_id = o.buyer_id
+LEFT JOIN Orders o
+    ON u.user_id = o.buyer_id
+   AND o.order_date >= '2019-01-01'
+   AND o.order_date < '2020-01-01'
+GROUP BY u.user_id, u.join_date
 ORDER BY u.user_id;
